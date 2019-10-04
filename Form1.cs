99e8823bff79e950e602601c28c8fa9fb39d3831
Wahyu2257 <48467871+Wahyu2257@users.noreply.github.com>
2019-10-04 10:25:05 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga1
+{
+    public partial class frmTulisanBerjalan : Form
+    {
+        public frmTulisanBerjalan()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblAnimasi.Left -= 10;
+            if (lblAnimasi.Left < -lblAnimasi.Width) lblAnimasi.Left = this.Width;
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
